fb00c4e291aa5f1702c52a2e3847ff9be47510e1
Silviu Dilimot <silviudilimot@gmail.com>
2021-03-26 23:48:15 +0100

    Weather App WITH process.env.API_ID

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import * as ELEMENTS from 'elements.js';
 import {Http} from 'http.js';
 import {WeatherData, WEATHER_PROXY_HANDLER} from 'weather-data.js'
 
-const APP_ID = 'c87d4361ac7902d363a4fb4527a5295c';
+const APP_ID = process.env.API_ID;
 
 ELEMENTS.SEARCH_BUTTON.addEventListener('click', searchWeather);
 
