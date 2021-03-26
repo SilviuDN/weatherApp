6243e2128710b86a08bff6294929df7d62768f16
Silviu Dilimot <silviudilimot@gmail.com>
2021-03-26 23:31:03 +0100

    Weather App WITH API_KEY

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import * as ELEMENTS from 'elements.js';
 import {Http} from 'http.js';
 import {WeatherData, WEATHER_PROXY_HANDLER} from 'weather-data.js'
 
-const APP_ID = 'API_KEY_GOES_HERE';
+const APP_ID = 'c87d4361ac7902d363a4fb4527a5295c';
 
 ELEMENTS.SEARCH_BUTTON.addEventListener('click', searchWeather);
 
